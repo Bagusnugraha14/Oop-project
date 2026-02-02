5555a79beb1db4ff7efc9d0ff76f6bc1eedc8629
Bagusnugraha14 <nugrahagede74@gmail.com>
2026-02-02 12:07:40 +0800

    ubah what is sama page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 "use client";
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./styles/main.module.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
--- src/app/whatis/page.js
+++ src/app/whatis/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import styles from "../styles/whatis.module.css";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 export default function whatis() {
   return (
